ecc575e0f3c76ae429616a0b144b25ed67624170
justdan6 <justdan2000@gmail.com>
2023-06-21 21:41:16 -0600

    Correcting image name in run-docker script

--- run-docker.sh
+++ run-docker.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --rm -p 0.0.0.0:8080:80 -p 0.0.0.0:8443:443/tcp -p 0.0.0.0:8443:443/udp -t dockerquarthypercornhttp3:latest
+docker run --rm -p 0.0.0.0:8080:80 -p 0.0.0.0:8443:443/tcp -p 0.0.0.0:8443:443/udp -t justdanz/quart-hypercorn-http3:latest
